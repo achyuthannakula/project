67ea00b838c9b1f9795114234a7cf2b9dd0ec47c
annakulaachyuth <annakulaachyuth@gmail.com>
2019-03-25 22:35:34 +0530

    initial commit

---
+++ client/src/util/Date.js
@@ -0,0 +1,50 @@
+export default date => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  date = new Date(parseInt(date));
+  const ourDate = new Date();
+  const dateObject = {
+    M: date.getMonth(),
+    d: date.getDate(),
+    h: date.getHours(),
+    m: date.getMinutes(),
+    y: date.getFullYear()
+  };
+  const ourDateObject = {
+    M: ourDate.getMonth(),
+    d: ourDate.getDate(),
+    h: ourDate.getHours(),
+    m: ourDate.getMinutes(),
+    y: ourDate.getFullYear()
+  };
+  //console.log(date, "-", ourDate, dateObject, ourDateObject);
+  if (dateObject.y !== ourDateObject.y)
+    return monthNames[dateObject.M] + " " + dateObject.y;
+  else if (dateObject.M !== ourDateObject.M)
+    return monthNames[dateObject.M] + " " + dateObject.d;
+  else {
+    let diff = 0;
+    if (dateObject.d !== ourDateObject.d) {
+      diff =
+        (ourDateObject.d - dateObject.d) * 24 - dateObject.h + ourDateObject.h;
+      if (diff >= 24) return dateObject.d + ", " + monthNames[dateObject.M];
+      else if (diff !== 0) return diff + " hr's ago";
+    }
+    diff =
+      (ourDateObject.h - dateObject.h) * 60 - dateObject.m + ourDateObject.m;
+    if (diff > 60) return ourDateObject.h - dateObject.h + " hr's ago";
+    return diff + " min's ago";
+  }
+};
---
+++ client/src/util/socialShare.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import Menu from "@material-ui/core/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import ShareIcon from "@material-ui/icons/Share";
+import MenuItem from "@material-ui/core/MenuItem";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  LinkedinIcon,
+  EmailIcon
+} from "react-share";
+
+class SocialShare extends Component {
+  state = {
+    anchorEl: null
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    console.log(this.props);
+
+    return (
+      <>
+        <IconButton
+          aria-owns={anchorEl ? "simple-menu" : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <Tooltip title="Share">
+            <ShareIcon />
+          </Tooltip>
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem>
+            <FacebookShareButton quote={this.props.title} url={this.props.url}>
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+          </MenuItem>
+          <MenuItem>
+            <LinkedinShareButton title={this.props.title} url={this.props.url}>
+              <LinkedinIcon size={32} round={true} />
+            </LinkedinShareButton>
+          </MenuItem>
+          <MenuItem>
+            <WhatsappShareButton title={this.props.title} url={this.props.url}>
+              <WhatsappIcon size={32} round={true} />
+            </WhatsappShareButton>
+          </MenuItem>
+          <MenuItem>
+            <TwitterShareButton title={this.props.title} url={this.props.url}>
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+          </MenuItem>
+          <MenuItem>
+            <EmailShareButton
+              subject={this.props.title}
+              body={this.props.url}
+              url={this.props.url}
+            >
+              <EmailIcon size={32} round={true} />
+            </EmailShareButton>
+          </MenuItem>
+        </Menu>
+      </>
+    );
+  }
+}
+
+export default SocialShare;
